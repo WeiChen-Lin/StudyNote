4688c6910338491ae6f185a83b398c36b84ee06a
WeiChen Lin <asdfg09487@gmail.com>
2023-05-21 23:55:23 +0800

    change title

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -57,7 +57,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'WeiChen 讀書筆記',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
